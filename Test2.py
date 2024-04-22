df340068c4fee3d913f1787cf8300060d560cc99
lstichler <164908768+lstichler@users.noreply.github.com>
2024-04-22 16:04:21 +0200

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("Displaying Text with Streamlit")
-    st.header("St. Gallen")
+    st.header("St. Gall0")
     
 if __name__ == "__main__":
     main()
