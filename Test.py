fd8cb9577141760b881a33eb2e022d984b162019
lstichler <164908768+lstichler@users.noreply.github.com>
2024-05-10 13:53:37 +0200

    Update Test BITTE.py

--- Test.py
+++ Test.py
@@ -23,7 +23,10 @@ def get_restaurants(location):
     url = "https://api.yelp.com/v3/businesses/search"
     headers = {
         "Authorization": "Bearer 6fvAUhr3oMOOOEmGybAjxAoqlAmWx31FhbvGnWw5R8jIhAfvIZVSXmT4GFYMeJsGKwb-0zX_pfD_CMpIVtEzHBdZ10EZ-fvzHkb_PYhtiJ9BHx4Ng359IGfz8Ak-ZnYx"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
+        "accept": "application/json"
     }
+
     params = {
         "term": "restaurants",
         "location": location
